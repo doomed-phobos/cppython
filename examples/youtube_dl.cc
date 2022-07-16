0051ee9ca52be8658faade191151c9f1ffc213cf
doomed-phobos <the_ekj.2121@outlook.com>
2022-07-15 21:46:58 -0500

    Update example

--- examples/youtube_dl.cc
+++ examples/youtube_dl.cc
@@ -5,7 +5,8 @@
 class App : public cppython::Application {
 public:
    virtual int run() override {
-      auto mod = cppython::Import::Make("youtube_dl.YoutubeDL", {"YoutubeDL"});
+      using namespace cppython::types;
+      auto mod = cppython::Import::Make("youtube_dl.YoutubeDL", {String("YoutubeDL")});
       if(!mod) {
          puts("Load youtube_dl.YoutubeDL failed");
          return 1;
@@ -17,13 +18,15 @@ public:
          return 1;
       }
 
-      auto ydl = ydl_class.callObject();
+      Dict d;
+      d.addItem("verbose", Bool(true));
+      Tuple t(d);
+      auto ydl = ydl_class.callObject(t);
       if(!ydl) {
          puts("YoutubeDL failed to instancing");
          return 1;
       }
-      ydl.callMethod("extract_info", "https://www.youtube.com/watch?v=-FMcAInEWDc", true);
-      // TODO : Añadir tupla para crear Object
+      ydl.callMethod("extract_info", String("https://www.youtube.com/watch?v=-FMcAInEWDc"), Bool(false));
 
       return 0;
    }
@@ -31,30 +34,5 @@ public:
 
 int main(int argc, char* argv[]) {
    App app;
-   /*
-   PyObject* ydl_object = PyObject_GetAttrString(module, "YoutubeDL");
-
-   auto tuple = PyTuple_New(1);
-   auto dict = PyDict_New();
-   auto value = PyBool_FromLong(1);
-   PyDict_SetItemString(dict, "quiet", value);
-   PyTuple_SetItem(tuple, 0, dict);
-   PyObject* ydl = PyObject_CallObject(ydl_object, tuple);           <==================     Este valor
-   Py_DECREF(value);
-   Py_DECREF(dict);
-   Py_DECREF(tuple);
-
-   Py_DECREF(ydl_object);
-   PyObject* video_info = PyObject_CallMethod(ydl, "extract_info", "s i", "https://www.youtube.com/watch?v=-FMcAInEWDc", 0);
-   if(PyErr_Occurred()) {
-      PyErr_Print();
-      return 1;
-   }
-   std::cout << PyList_CheckExact(video_info) << std::endl;
-
-   Py_DECREF(ydl);*/
-   
-   // Finalizing
-
    return app.run();
 }

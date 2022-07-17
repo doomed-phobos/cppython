0a0765a67fb0d776eb3ddf57c4437569a34c456a
doomed-phobos <the_ekj.2121@outlook.com>
2022-07-17 11:36:35 -0500

    Update youtube_dl

--- examples/youtube_dl.cc
+++ examples/youtube_dl.cc
@@ -1,7 +1,6 @@
 #include "cppython/cppython.hpp"
 
 #include <iostream>
-#include <Python.h>
 
 class App : public cppython::Application {
 public:
@@ -9,13 +8,13 @@ public:
       using namespace cppython::types;
       auto mod = cppython::load_module("youtube_dl.YoutubeDL", {String("YoutubeDL")});
       if(!mod) {
-         puts("Load youtube_dl.YoutubeDL failed");
+         CPPYTHON_SHOW_ERROR();
          return 1;
       }
 
       auto ydl_class = mod.getAttr("YoutubeDL").to<cppython::Callable>();
       if(!ydl_class) {
-         puts("YoutubeDL class don't exists");
+         CPPYTHON_SHOW_ERROR();
          return 1;
       }
       Dict d({
@@ -23,20 +22,20 @@ public:
          {"simulate", Bool(true)}});
       auto ydl = ydl_class.callObject(Tuple({d}));
       if(!ydl) {
-         puts("YoutubeDL failed to instancing");
+         CPPYTHON_SHOW_ERROR();
          return 1;
       }
 
       auto video_info = ydl.callMethod("extract_info",
          Tuple{String("https://www.youtube.com/watch?v=-FMcAInEWDc"), Bool(false)}).to<Dict>();
       if(!video_info) {
-         puts("Error to get video info");
+         CPPYTHON_SHOW_ERROR();
          return 1;
       }
 
       auto format = video_info.getItem("requested_formats").to<Tuple>().getItem(0).to<Dict>();
       if(!format) {
-         puts("Error to get format");
+         CPPYTHON_SHOW_ERROR();
          return 1;
       }
       std::cout << format.getItem("url").to<String>().value() << std::endl;

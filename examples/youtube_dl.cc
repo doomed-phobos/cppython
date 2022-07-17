dcf23376f16e383015a727ae1bac88b5c37f5d97
doomed-phobos <the_ekj.2121@outlook.com>
2022-07-17 09:17:22 -0500

    Update youtube_dl

--- examples/youtube_dl.cc
+++ examples/youtube_dl.cc
@@ -6,27 +6,30 @@ class App : public cppython::Application {
 public:
    virtual int run() override {
       using namespace cppython::types;
-      auto mod = cppython::Import::Make("youtube_dl.YoutubeDL", {String("YoutubeDL")});
+      auto mod = cppython::Module::Make("youtube_dl.YoutubeDL", {String("YoutubeDL")});
       if(!mod) {
          puts("Load youtube_dl.YoutubeDL failed");
          return 1;
       }
 
-      auto ydl_class = mod.getAttr("YoutubeDL");
+      auto ydl_class = mod.getAttr("YoutubeDL").to<cppython::Callable>();
       if(!ydl_class) {
          puts("YoutubeDL class don't exists");
          return 1;
       }
-
-      Dict d;
-      d.addItem("verbose", Bool(true));
-      Tuple t(d);
-      auto ydl = ydl_class.callObject(t);
+      Dict d({
+         {"verbose", Bool(true)}});
+      auto ydl = ydl_class->callObject(Tuple({d}));
       if(!ydl) {
          puts("YoutubeDL failed to instancing");
          return 1;
       }
-      ydl.callMethod("extract_info", String("https://www.youtube.com/watch?v=-FMcAInEWDc"), Bool(false));
+
+      auto video_info = ydl.callMethod("extract_info", String("https://www.youtube.com/watch?v=-FMcAInEWDc"), Bool(false));
+      if(!video_info) {
+         puts("Error to get video info");
+         return 1;
+      }
 
       return 0;
    }

9269808a17e1a7598cd352603a5b5167c091cc61
doomed-phobos <the_ekj.2121@outlook.com>
2022-07-17 09:15:38 -0500

    Update Object
    * Added casting

--- include/cppython/object.hpp
+++ include/cppython/object.hpp
@@ -1,16 +1,15 @@
 #pragma once
-#include "cppython/ctype_traits.hpp"
+#include <optional>
 
-#include <Python.h>
+typedef struct _object PyObject;
 
 namespace cppython {
-   namespace types {
-      class Tuple;
-   }
-
    class Object {
    public:
-      Object(PyObject* pyObj);
+      ///
+      /// Make a empty object
+      ///
+      Object();
       Object(const Object& o);
       Object(Object&& o);
       virtual ~Object();
@@ -19,20 +18,39 @@ namespace cppython {
 
       bool isValid() const;
       unsigned refCnt() const;
-      Object callObject();
-      Object callObject(const types::Tuple& args);
 
-      template<typename R,typename... Args>
-      std::enable_if_t<types::is_ctype_v<R>, R> callMethod(const char* name, Args&&... args);
+      ///
+      /// Cast Object to Derived only if is the same type.
+      ///
+      template<typename Derived,
+         std::enable_if_t<
+            std::is_base_of_v<Object, Derived> && std::is_same_v<decltype(std::declval<Derived>().CheckType), bool(const Object&)>, int> = 0>
+      std::optional<Derived> to() {
+         if(!isValid() || !Derived::CheckType(*this))
+            return std::nullopt;
+         
+         Derived d;
+         d.swap(*this);
+         return d;
+      }
+
+      ///
+      /// Create a Object and unref when calling destructor
+      ///
+      static Object MakeFromRaw(PyObject* pyObj);
+      ///
+      /// Just wrap a PyObject*, without unref when calling destructor
+      ///
+      static Object MakeFromBorrow(PyObject* pyObj);
 
       Object& operator=(const Object& o);
       Object& operator=(Object&& o);
       operator PyObject*() const {return m_pyObj;}
       operator bool() const {return isValid();}
+   protected:
+      Object(PyObject* pyObj, bool unref = true);
    private:
       PyObject* m_pyObj;
+      bool m_unref;
    };
 } // namespace cppython
-
-#include "cppython/ctypes-inl.hpp"
-#include "cppython/object-inl.hpp"
